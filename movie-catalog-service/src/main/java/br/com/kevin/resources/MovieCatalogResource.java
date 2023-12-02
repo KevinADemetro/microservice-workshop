f5aa234bc4a473bf2cb5421bd773b6a514f984b9
Kevin Albino Demetro <albinodemetrok@gmail.com>
2023-12-02 00:15:21 -0300

    Criando bean RestTemplate

--- movie-catalog-service/src/main/java/br/com/kevin/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/br/com/kevin/resources/MovieCatalogResource.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +19,9 @@ import br.com.kevin.models.Rating;
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
+	
+	@Autowired
+	private RestTemplate restTemplate;
 	
 	@GetMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
